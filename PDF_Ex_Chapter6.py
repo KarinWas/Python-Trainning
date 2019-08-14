4667995373a786385c07004c3b44a070f24e03ec
danak1995@gmail.com <karinw2512@gmail.com>
2019-08-14 16:28:53 +0300

    Finished 6.6 Ex - without 6.5

--- PDF_Ex_Chapter6.py
+++ PDF_Ex_Chapter6.py
@@ -20,6 +20,10 @@ def list_to_dictionary(lst):
         cnt[value] += 1
     return cnt
 
+#Ex 6.6
+def reverse_look_up(dict, searchValue):
+    return (list(dict.keys())[list(dict.values()).index(searchValue)])
+
 if __name__== "__main__":
     marbles = {"red": 34, "green": 30, "brown": 31, "yellow": 29 }
     print_dictionary(marbles)
@@ -30,3 +34,4 @@ if __name__== "__main__":
     yellow 29'''
     names = ['Karin', 'Eli', 'Dana', 'Karin', 'Karin', 'Dana', 'Eden']
     print(list_to_dictionary(names)) # answer : {'Karin': 3, 'Eli': 1, 'Dana': 2, 'Eden': 1}
+    print(reverse_look_up(marbles, 31)) #answer: brown
